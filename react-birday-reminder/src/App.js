22eea88e0037535dfdc0c27c5239ab9435c0f63f
frontendprof <abdumaliksharipov@gmail.com>
2020-11-07 21:43:03 +0500

    Applying style on components

--- react-birday-reminder/src/App.js
+++ react-birday-reminder/src/App.js
@@ -1,11 +1,18 @@
-import './App.css';
+
+
+import React, { useState } from 'react';
+import data from './data';
+import List from './List';
+
 
 function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+  return <main>
+    <section className="container">
+      <h3>0 birthday today</h3>
+      <List />
+      <button onClick={() => console.log("Stop clicking very hard")}>Clear All</button>
+    </section>
+  </main >
 }
 
 export default App;
