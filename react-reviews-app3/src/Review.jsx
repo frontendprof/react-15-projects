1338184de6995430221c6f8e13ccd48b50c5c655
frontendprof <abdumaliksharipov@gmail.com>
2020-11-08 20:12:28 +0500

    Initial setup for react-reviews

---
+++ react-reviews-app3/src/Review.jsx
@@ -0,0 +1,12 @@
+
+import React, { useState } from 'react';
+import people from './data';
+import { FaChevronLeft, FaChevronRight, FaQuoteRight } from 'react-icons/fa';
+
+
+
+const Review = () => {
+    return <h2>review component</h2>;
+};
+
+export default Review;
