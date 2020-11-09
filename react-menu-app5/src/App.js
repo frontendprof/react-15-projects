de4d1deb6ec0cf04ba7205872ad768a7971b87a6
frontendprof <abdumaliksharipov@gmail.com>
2020-11-09 17:00:51 +0500

    Destructuring food data and demonstarting it

--- react-menu-app5/src/App.js
+++ react-menu-app5/src/App.js
@@ -7,7 +7,23 @@ import items from './data';
 
 
 function App() {
-  return <h2>menu project setup</h2>;
+  const [menuItems, setMenuItems] = useState(items);
+  const [categories, setCategories] = useState([]);
+
+
+  return (
+    <main>
+      <section className="menu section">
+        <div className="title">
+          <h2>our menu</h2>
+          <div className="underline"></div>
+        </div>
+
+        <Categories />
+        <Menu items={menuItems} />
+      </section>
+    </main>
+  )
 }
 
 export default App;
