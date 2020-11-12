09343138968a4e0435ba166ab2ec7ef7c5c80021
frontendprof <abdumaliksharipov@gmail.com>
2020-11-12 13:29:20 +0500

    Workng on prev && next btn functionalities

--- react-slider-app7/src/App.js
+++ react-slider-app7/src/App.js
@@ -11,6 +11,20 @@ function App() {
   const [people, setPeople] = useState(data);
   const [index, setIndex] = useState(0);
 
+
+  useEffect(() => {
+    const lastIndex = people.length - 1;
+
+    if (index < 0) {
+      setIndex(lastIndex)
+    }
+    if (index > lastIndex) {
+      setIndex(0)
+    }
+  }, [index, people])
+
+
+
   return (
     <section className="section">
       <div className="title">
@@ -23,8 +37,16 @@ function App() {
         {people.map((person, personIndex) => {
           const { id, image, name, title, quote } = person;
 
+          let position = "nextSlide";
+          if (personIndex === index) {
+            position = "activeSlide";
+          }
+          if (personIndex === index - 1 || (index === 0 && personIndex === people.length - 1)) {
+            position = "lastSlide";
+          }
+
           return (
-            <article key={id}>
+            <article className={position} key={id}>
               <img src={image} alt={title} className="person-img" />
               <h4>{name}</h4>
               <p className="title">{title}</p>
@@ -34,8 +56,8 @@ function App() {
           )
         })}
 
-        <button className="prev"><FiChevronLeft /></button>
-        <button className="next"><FiChevronRight /></button>
+        <button className="prev" onClick={() => setIndex(index - 1)}><FiChevronLeft /></button>
+        <button className="next" onClick={() => setIndex(index + 1)}><FiChevronRight /></button>
       </div>
     </section>
   )
