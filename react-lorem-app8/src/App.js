580b99ede062ef775ff1511cec7b5d99ad67e598
frontendprof <abdumaliksharipov@gmail.com>
2020-11-12 13:59:33 +0500

    Initializing lorem app

---
+++ react-lorem-app8/src/App.js
@@ -0,0 +1,16 @@
+import React, { useState } from 'react';
+import data from './data';
+
+
+
+
+
+function App() {
+  
+  
+  return (
+  <h2>lorem ipsum project setup</h2>
+    )
+}
+
+export default App;
